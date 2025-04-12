34c065683851b7e87504e4a48594ccfafd3e46c6
22120169 <nguyenkieniski@gmail.com>
2025-04-13 02:58:26 +0700

    tính toán lại đường đi cho ma
    -cập nhật lại để không init ma vào khoảng rỗng trên maze
    -cho phép đi thông qua đường hầm ở hàng 14

--- entities/blueGhost.py
+++ entities/blueGhost.py
@@ -9,9 +9,16 @@ class BlueGhost:
 
     def get_random_position(self):
         empty_cells = []
+        restricted_rows = {10, 11, 12, 16, 17, 18}
+        restricted_cols_start = set(range(5))
+        restricted_cols_end = set(range(22, 28))
+
         for row_index, row in enumerate(MAZE_LAYOUT):
             for col_index, cell in enumerate(row):
-                if cell == '0':
+                if cell == '0': 
+                    if row_index in restricted_rows:
+                        if col_index in restricted_cols_start or col_index in restricted_cols_end:
+                            continue  
                     empty_cells.append((col_index, row_index))
         return random.choice(empty_cells)
 
@@ -22,39 +29,57 @@ class BlueGhost:
     def find_path_to_pacman(self, target_x, target_y):
         visited = set()
         parent_map = {}
-
         queue = deque([(self.x, self.y)])
         visited.add((self.x, self.y))
-
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         while queue:
             current_x, current_y = queue.popleft()
 
             if (current_x, current_y) == (target_x, target_y):
-                # Truy ngược đường đi từ Pacman đến ma quái
                 self.path = []
                 while (current_x, current_y) != (self.x, self.y):
                     self.path.append((current_x, current_y))
                     current_x, current_y = parent_map[(current_x, current_y)]
                 self.path.append((self.x, self.y))
-                self.path.reverse()  # Đảo ngược để có đường đi từ ma quái đến Pacman
+                self.path.reverse()
                 return
+
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            if current_y == 14:
+                if current_x == 0:
+                    directions.append((-1, 0))
+                if current_x == 27:
+                    directions.append((1, 0))
 
             for dx, dy in directions:
                 next_x, next_y = current_x + dx, current_y + dy
+                if current_y == 14:
+                    if current_x == 0 and dx == -1:
+                        next_x = 27
+                    elif current_x == 27 and dx == 1:
+                        next_x = 0 
                 if self.is_valid(next_x, next_y) and (next_x, next_y) not in visited:
                     visited.add((next_x, next_y))
                     parent_map[(next_x, next_y)] = (current_x, current_y)
                     queue.append((next_x, next_y))
 
+        self.path = []
+
     def is_valid(self, x, y):
+        if y == 14 and (x == -1 or x == 28):
+            return True
         return 0 <= y < len(MAZE_LAYOUT) and 0 <= x < len(MAZE_LAYOUT[0]) and MAZE_LAYOUT[y][x] == '0'
 
     def move_step(self):
         if self.path and len(self.path) > 1:
-            self.path.pop(0)  # Xóa bước hiện tại
-            self.x, self.y = self.path[0]  # Di chuyển đến bước tiếp theo
+            self.path.pop(0)
+            next_x, next_y = self.path[0]
+            if next_y == 14:
+                if next_x == -1:
+                    next_x = 27
+                elif next_x == 28:
+                    next_x = 0
+            self.x, self.y = next_x, next_y
 
     def draw(self, screen, tile_size):
         import pygame
--- entities/pinkGhost.py
+++ entities/pinkGhost.py
@@ -8,9 +8,16 @@ class PinkGhost:
 
     def get_random_position(self):
         empty_cells = []
+        restricted_rows = {10, 11, 12, 16, 17, 18}
+        restricted_cols_start = set(range(5))
+        restricted_cols_end = set(range(22, 28))
+
         for row_index, row in enumerate(MAZE_LAYOUT):
             for col_index, cell in enumerate(row):
                 if cell == '0':
+                    if row_index in restricted_rows:
+                        if col_index in restricted_cols_start or col_index in restricted_cols_end:
+                            continue
                     empty_cells.append((col_index, row_index))
         return random.choice(empty_cells)
 
@@ -28,23 +35,46 @@ class PinkGhost:
                 return True
             if (x, y) in visited or not self.is_valid(x, y):
                 return False
+            
             visited.add((x, y))
-            for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-                if dfs(x + dx, y + dy):
+
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            if y == 14:
+                if x == 0:
+                    directions.append((-1, 0))
+                if x == 27:
+                    directions.append((1, 0)) 
+            for dx, dy in directions:
+                next_x, next_y = x + dx, y + dy
+                if y == 14:
+                    if x == 0 and dx == -1:
+                        next_x = 27
+                    elif x == 27 and dx == 1:
+                        next_x = 0 
+                if dfs(next_x, next_y):
                     path.append((x, y))
                     return True
             return False
 
         if dfs(self.x, self.y):
-            self.path = path[::-1]  # reverse to get from ghost to pacman
+            self.path = path[::-1] 
 
     def is_valid(self, x, y):
+        if y == 14 and (x == -1 or x == 28):
+            return True
         return 0 <= y < len(MAZE_LAYOUT) and 0 <= x < len(MAZE_LAYOUT[0]) and MAZE_LAYOUT[y][x] == '0'
 
     def move_step(self):
         if self.path and len(self.path) > 1:
             self.path.pop(0)
-            self.x, self.y = self.path[0]
+            next_x, next_y = self.path[0]
+            if next_y == 14:
+                if next_x == -1:
+                    next_x = 27  
+                elif next_x == 28:
+                    next_x = 0  
+            self.x, self.y = next_x, next_y
 
     def draw(self, screen, tile_size):
         import pygame
