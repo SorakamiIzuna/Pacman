54105e3ab174a8179489273468fd338696ad8220
22120010 <hoangtuananh2004.work@gmail.com>
2025-04-19 16:30:47 +0700

    initial expanded nodes testing

---
+++ test/expanded_nodes_testing.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from entities.pacman import Pacman
+from entities.blueGhost import BlueGhost
+from entities.orangeGhost import OrangeGhost 
+from entities.pinkGhost import PinkGhost
+from entities.redGhost import RedGhost
